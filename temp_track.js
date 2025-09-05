a36ef1cb4eec38d2e8e0e7dd5a2d4a8a40e4e337
zpandeva <zoya>
2025-09-05 12:03:40 +0300

    Addres review comments

--- temp_track.js
+++ temp_track.js
@@ -1,7 +1,9 @@
-/* shelly model Shelly 2PM Gen3s
- */
+//  SPDX-License-Identifier: GPL-2.0-or-later
+//  Copyright (C) 2025, Zoya Pandeva <zoya.pandeva@gmail.com>
+//  shelly model Shelly 2PM Gen3s
 
 let CONFIG = {
+  debug: true,
   scanInterval_ms: 5000, // miliseconds: read the sensors on every 5000 ms
   tempThreshold: 3, // °C
   switch: { id: 0, name: "heater" },
@@ -12,14 +14,16 @@ function tempTracker() {
     Shelly.call(
       "Switch.GetStatus",
       { id: CONFIG.switch.id },
-      function (result, err_code, err_message) { 
+      function (result, err_code, err_message) {
         if (result !== undefined && err_code === 0) {
-          console.log(
-            "Current heater status is",
-            onOffState(result.output),
-            err_code,
-            err_message
-          );
+          if (CONFIG.debug) {
+            console.log(
+              "Current heater status is",
+              onOffState(result.output),
+              err_code,
+              err_message
+            );
+          }
           const currentState = result.output;
           let temp = readTempSensor();
           setSwitch(temp, currentState);
@@ -38,23 +42,21 @@ function readTempSensor() {
 }
 
 function setSwitch(temp, currentState) {
-  print(temp);
   let desireState = temp < CONFIG.tempThreshold;
   if (currentState !== desireState) {
     Shelly.call("Switch.Set", { id: CONFIG.switch.id, on: desireState });
   }
-  console.log("Heater is ", onOffState(desireState));
+  if (CONFIG.debug) {
+    console.log("Heater is ", onOffState(desireState));
+  }
 }
+
 function onOffState(state) {
   return state ? "On" : "Off";
 }
 
-function onUserCommand(request, response) {}
-
 //init the script
 function init() {
-  HTTPServer.registerEndpoint("user_command", onUserCommand);
-
   //start the timer
   Timer.set(CONFIG.scanInterval_ms, true, tempTracker);
 
